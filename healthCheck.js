e6154050065996345259699c748cf3747b656e3a
gaodashang167 <160224837+gaodashang167@users.noreply.github.com>
2026-01-29 22:58:36 +0800

    Update healthCheck.js

--- healthCheck.js
+++ healthCheck.js
@@ -7,7 +7,7 @@ app.get('/health', (req, res) => {
 });
 
 // 启动 HTTP 服务器
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Health check server running on port ${PORT}`);
 });
